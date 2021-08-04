d8e233603c7904876e9b695fd01b887cc809bf3a
Caner Al <caneral5858@gmail.com>
2021-08-04 19:31:19 +0300

    Header eklendi, düzenlemeleri yapılıyor.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <div>
-      
+      <div className="bg-red-700 min-h-screen ">
+        <div className="bg-white max-w-6xl  mx-auto">
+        <Header/>
+        </div>
+      </div>
     </div>
   )
 }
