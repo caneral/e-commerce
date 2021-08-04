7cd45d4bf7c328e20df833b7d6f42e578aa72899
Caner Al <caneral5858@gmail.com>
2021-08-05 01:22:31 +0300

    Header güncellendi

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header'
 const App = () => {
   return (
     <div>
-      <div className="bg-red-700 min-h-screen ">
+      <div className="min-h-screen ">
         <div className="bg-white max-w-6xl  mx-auto">
         <Header/>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          base: "#919191",
+        },
+        orange: {
+          base: "#ff6000",
+        }
+      }
+    },
   },
   variants: {
     extend: {},
