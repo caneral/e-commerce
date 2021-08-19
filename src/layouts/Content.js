5fa38fd693346999c0408f17266e942f6660107d
Caner Al <caneral5858@gmail.com>
2021-08-19 15:25:00 +0300

    Anasayfa keşfet kısmı oluşturuldu.

--- src/layouts/Content.js
+++ src/layouts/Content.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import HomeSlider from '../components/HomeSlider'
+import Discover from '../components/Discover'
 const Content = () => {
     return (
         <div>
             <HomeSlider/>
+            <Discover/>
         </div>
     )
 }
